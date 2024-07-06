c441c4437feef595427b245a2bc77544b63123d5
Jerry <jerryfung110@gmail.com>
2024-07-07 00:24:36 +0100

    added function

--- src/Trial.jl
+++ src/Trial.jl
@@ -1,5 +1,24 @@
 module Trial
 
-# Write your package code here.
+import Downloads
+import ExaModels: ExaModels, NLPModels
 
+include("rosenrock.jl")
+include("wood.jl")
+include("cragg_levy.jl")
+include("broyden_banded.jl")
+include("broyden_tridiagonal.jl")
+include("chained_power.jl")
+
+const NAMES = filter(names(optimizer; all = true)) do x
+    str = string(x)
+    endswith(str, "model") && !startswith(str, "#")
 end
+
+for name in NAMES
+    @eval export $name
+end
+
+
+
+end # module Trail
