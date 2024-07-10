0b94e661aaaa905a10f5c8b0a6f7840fac7693b7
Jerry <jerryfung110@gmail.com>
2024-07-10 20:00:01 +0100

    Test CI

--- src/Trial.jl
+++ src/Trial.jl
@@ -3,16 +3,17 @@ module Trial
 import Downloads
 import ExaModels: ExaModels, NLPModels
 
-include("rosenrock.jl")
+include("rosenrock.jl") 
 #include("wood.jl")
 include("cragg_levy.jl")
 #include("broyden_banded.jl")
-#include("broyden_tridiagonal.jl")
+include("broyden_tridiagonal.jl")
 include("chained_powell.jl")
 include("augmented_lagrangian.jl")
 include("trigo_tridiagonal.jl")
 include("modified_brown.jl")
 include("generalized_brown.jl")
+println("HI")
 const NAMES = filter(names(Trial; all = true)) do x
     str = string(x)
     endswith(str, "model") && !startswith(str, "#")
