6766557d6b04486dbc001ff1db27d539ea7fb38c
Jerry <jerryfung110@gmail.com>
2024-07-12 20:12:36 +0100

    find bug in wood

--- src/wood.jl
+++ src/wood.jl
@@ -1,24 +1,22 @@
-
-
 function wood_model(N = 10; T = Float64, kwargs...)
     c = ExaModels.ExaCore(T)
     x = ExaModels.variable(c, N; start = (mod(i, 2) == 1 ? -2 : 0 for i = 1:N))
-    
+    println(1)
     # Define the constraints
     con = ExaModels.constraint(
         c,
-        (2 + 5 * x[(k)+5]^2) * x[(k)+5] + 1 for k in 1:N-7
+        (2 + 5 * x[k+5]^2) * x[k+5] + 1 for k in 1:N-7
     )
-    
+    println(2)
     ExaModels.constraint!(
         c,
         con,
         i => x[i] * (1 + x[i]) + x[i+1] * (1 + x[i+1]) for i in 1:N-7
     )
-
+    println(3)
     ExaModels.objective(c, (100 * (x[2*i-1]^2 - x[2*i])^2 + (x[2*i-1] - 1)^2 +
     90 * (x[2*i+1]^2 - x[2*i+2])^2 + (x[2*i+1] - 1)^2 +
-    10 * (x[2*i] + x[2*i+2] - 2)^2 + (x[2*i] - x[2*i+2])^2 / 10) for i = 1:Int(N/2))
+    10 * (x[2*i] + x[2*i+2] - 2)^2 + ((x[2*i] - x[2*i+2])^2 / 10)) for i = 400:Int(N/2))
     
     return ExaModels.ExaModel(c)
 end
