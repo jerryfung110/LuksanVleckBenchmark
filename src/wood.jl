cddb9d2524767d66ea7964dda59612494a8878cd
Jerry <jerryfung110@gmail.com>
2024-07-12 20:05:51 +0100

    manually did wood

--- src/wood.jl
+++ src/wood.jl
@@ -1,12 +1,24 @@
-function wood_model(N = 1000; T = Float64, backend = CUDABackend(), kwargs ...)
-    c = ExaModels.ExaCore(T; backend = backend)
+
+
+function wood_model(N = 10; T = Float64, kwargs...)
+    c = ExaModels.ExaCore(T)
     x = ExaModels.variable(c, N; start = (mod(i, 2) == 1 ? -2 : 0 for i = 1:N))
-    ExaModels.constraint(
+    
+    # Define the constraints
+    con = ExaModels.constraint(
         c,
-        (2 + 5 * x[k+5]^2) * x[k+5] + 1 + sum(x[i] * (1 + x[i]) for i in k-5:k+1) for k = 1:N-7
+        (2 + 5 * x[(k)+5]^2) * x[(k)+5] + 1 for k in 1:N-7
     )
+    
+    ExaModels.constraint!(
+        c,
+        con,
+        i => x[i] * (1 + x[i]) + x[i+1] * (1 + x[i+1]) for i in 1:N-7
+    )
+
     ExaModels.objective(c, (100 * (x[2*i-1]^2 - x[2*i])^2 + (x[2*i-1] - 1)^2 +
     90 * (x[2*i+1]^2 - x[2*i+2])^2 + (x[2*i+1] - 1)^2 +
-    10 * (x[2*i] + x[2*i+2] - 2)^2 + (x[2*i] - x[2*i+2])^2 / 10) for i =1:Int(N/2))
+    10 * (x[2*i] + x[2*i+2] - 2)^2 + (x[2*i] - x[2*i+2])^2 / 10) for i = 1:Int(N/2))
+    
     return ExaModels.ExaModel(c)
 end
