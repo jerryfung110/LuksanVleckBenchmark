b73b7d0637450388eac504ca697f3c73864e2d8b
jerryfung110 <jerryfung110@gmail.com>
2024-08-13 20:48:13 +0000

    added description to wood.jl

--- src/wood.jl
+++ src/wood.jl
@@ -1,3 +1,5 @@
+# The following question was adopted from Lukšan, L., & Vlček, J. (1999). Sparse and Partially Separable Test Problems for Unconstrained and Equality Constrained Optimization.
+# Institute of Computer Science, Academy of Sciences of the Czech Republic. Technical report No. 767 Problem 5.2
 function wood_model(N = 10; T = Float64, backend = CUDABackend(), kwargs...)
     c = ExaModels.ExaCore(T; backend = backend)
     x = ExaModels.variable(c, N; start = (mod(i, 2) == 1 ? -2 : 0 for i = 1:N))
